92ef6775f8262c909be0f54db0bccf1ee449bf07
unknown <nzamotaev@spb-NZamotaev.spb.luxoft.com>
2013-02-22 17:44:13 +0400

    Added analytic function templates

--- hp2dxy.c
+++ hp2dxy.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 
 void read_to_eoln() {
+	//This is temporary and wrong
 	int ch;
 	do{
 		ch=getchar();
 	}while(ch!='\n'||ch!='\r'&&ch>0);
 	if(ch>0) ungetchar(ch);
+};
+
+void analise_i() {
+};
+
+void analise_p() {
+};
+
+void analise_s() {
 };
 
 int main(void) {
@@ -15,14 +25,17 @@ int main(void) {
   	switch(ch){
 		case 'I':
 		case 'i':{
+			analise_i();
 			break;
 		};
 		case 'P':
 		case 'p':{
+			analise_p();
 			break;
 		};
 		case 'S':
 		case 's':{
+			analise_s();
 			break;
 		};
 		default:{
