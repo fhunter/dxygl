61ec8df8299111daf56469ef441d8e784a581eec
Nikolaj Zamotaev <fhunter@fhunter.mine.nu>
2013-03-15 03:29:31 +0400

    Reindent

--- optimizer/optimizer.c
+++ optimizer/optimizer.c
@@ -2,36 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct point_2d {
+struct point_2d
+{
   float x;
   float y;
 };
 
-struct line_2d {
+struct line_2d
+{
   struct point_2d begin;
   struct point_2d end;
 };
 
-float distance(struct point_2d point1, struct point_2d point2){
-	return sqrt(pow(point1.x-point2.x,2)+pow(point1.y-point2.y,2));
+float distance( struct point_2d point1, struct point_2d point2 )
+{
+  return sqrt( pow( point1.x - point2.x, 2 ) +
+               pow( point1.y - point2.y, 2 ) );
 };
 
-int main(void){
-  struct line_2d *lines=NULL;
-  int lines_count=0;
+int main( void )
+{
+  struct line_2d *lines = NULL;
+  int lines_count = 0;
   int i;
-  while(!feof(stdin)){
+  while( !feof( stdin ) ) {
     //read_line();
     //parse_line();
     //if this_is_pen_down() store_next_line_point();
   };
   //Sort lines;
   //Output lines;
-  for(i=0;i<lines_count;i++){
+  for( i = 0; i < lines_count; i++ ) {
     //Output continuous pen down instruction
     //Output pen_up
   };
   //somehow output everything else :D
-  free(lines);
+  free( lines );
   return 0;
 };
